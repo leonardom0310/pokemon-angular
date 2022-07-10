d1913d12a4721bad87b0cc7990410ee471bc1131
Leonardo Martins <leonardo.martins@projuris.com.br>
2022-07-10 12:40:32 -0300

    store-install

--- apps/desafio-frontend/src/app/app.module.ts
+++ apps/desafio-frontend/src/app/app.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { StoreModule } from '@ngrx/store';
 
 import { AppComponent } from './app.component';
 import { NxWelcomeComponent } from './nx-welcome.component';
 
 @NgModule({
   declarations: [AppComponent, NxWelcomeComponent],
-  imports: [BrowserModule],
+  imports: [BrowserModule, StoreModule.forRoot({})],
   providers: [],
   bootstrap: [AppComponent],
 })
